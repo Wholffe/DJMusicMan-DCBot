54ce77b3f6e8be668129dd6b1465fe2db53e3845
Wholffe <149335217+Wholffe@users.noreply.github.com>
2025-11-22 00:51:07 +0100

    edit localbuild_container script

--- scripts/linux/localbuild_container.sh
+++ scripts/linux/localbuild_container.sh
@@ -5,19 +5,19 @@ CONTAINER_NAME="djmusicman"
 DOCKER_IMAGE_NAME="djmusicman"
 DATA_PATH="$PROJECT_PATH/data"
 
-git pull origin main --force
+#git pull origin main --force
 
 mkdir -p "$DATA_PATH"
 chmod 777 "$DATA_PATH"
 
-sudo docker stop "$CONTAINER_NAME" || true
-sudo docker rm "$CONTAINER_NAME" || true
+docker stop "$CONTAINER_NAME" || true
+docker rm "$CONTAINER_NAME" || true
 docker build --no-cache -t "$DOCKER_IMAGE_NAME" .
-sudo docker run -d \
+docker run -d \
   --name "$CONTAINER_NAME" \
   --env-file .env \
   -v "$DATA_PATH":/data \
   --restart unless-stopped \
   "$DOCKER_IMAGE_NAME"
 
-sudo docker images --filter 'dangling=true' -q | xargs -r sudo docker rmi -f
+docker images --filter 'dangling=true' -q | xargs -r sudo docker rmi -f
