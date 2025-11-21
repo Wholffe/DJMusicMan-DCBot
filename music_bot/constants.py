c10b8b9c3dca5649765110b716498e70b32cbc3f
Wholffe <149335217+Wholffe@users.noreply.github.com>
2025-11-22 00:46:20 +0100

    edit version

--- music_bot/constants.py
+++ music_bot/constants.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 from typing import Final
 
 # region Info
-VERSION: Final = "2.2.5"
+VERSION: Final = "2.2.6"
 NAME: Final = "DJ Music Man"
 FOOTER: Final = f"{NAME} {VERSION}"
 
